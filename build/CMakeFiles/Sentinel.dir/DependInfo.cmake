ae472d1383745e4faca9617a84dd34f0255e693f
Menthings <menthy@protonmail.com>
2024-03-04 00:00:24 +0100

    Compile and try the script

---
+++ build/CMakeFiles/Sentinel.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/menthings/Projets/Sentinel/main.cpp" "CMakeFiles/Sentinel.dir/main.cpp.o" "gcc" "CMakeFiles/Sentinel.dir/main.cpp.o.d"
+  "/home/menthings/Projets/Sentinel/src/CpuMonitor.cpp" "CMakeFiles/Sentinel.dir/src/CpuMonitor.cpp.o" "gcc" "CMakeFiles/Sentinel.dir/src/CpuMonitor.cpp.o.d"
+  "/home/menthings/Projets/Sentinel/src/DiskMonitor.cpp" "CMakeFiles/Sentinel.dir/src/DiskMonitor.cpp.o" "gcc" "CMakeFiles/Sentinel.dir/src/DiskMonitor.cpp.o.d"
+  "/home/menthings/Projets/Sentinel/src/NetworkMonitor.cpp" "CMakeFiles/Sentinel.dir/src/NetworkMonitor.cpp.o" "gcc" "CMakeFiles/Sentinel.dir/src/NetworkMonitor.cpp.o.d"
+  "/home/menthings/Projets/Sentinel/src/RamMonitor.cpp" "CMakeFiles/Sentinel.dir/src/RamMonitor.cpp.o" "gcc" "CMakeFiles/Sentinel.dir/src/RamMonitor.cpp.o.d"
+  "/home/menthings/Projets/Sentinel/src/SystemMonitor.cpp" "CMakeFiles/Sentinel.dir/src/SystemMonitor.cpp.o" "gcc" "CMakeFiles/Sentinel.dir/src/SystemMonitor.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Sentinel.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sentinel.dir/main.cpp.o"
+  "CMakeFiles/Sentinel.dir/main.cpp.o.d"
+  "CMakeFiles/Sentinel.dir/src/CpuMonitor.cpp.o"
+  "CMakeFiles/Sentinel.dir/src/CpuMonitor.cpp.o.d"
+  "CMakeFiles/Sentinel.dir/src/DiskMonitor.cpp.o"
+  "CMakeFiles/Sentinel.dir/src/DiskMonitor.cpp.o.d"
+  "CMakeFiles/Sentinel.dir/src/NetworkMonitor.cpp.o"
+  "CMakeFiles/Sentinel.dir/src/NetworkMonitor.cpp.o.d"
+  "CMakeFiles/Sentinel.dir/src/RamMonitor.cpp.o"
+  "CMakeFiles/Sentinel.dir/src/RamMonitor.cpp.o.d"
+  "CMakeFiles/Sentinel.dir/src/SystemMonitor.cpp.o"
+  "CMakeFiles/Sentinel.dir/src/SystemMonitor.cpp.o.d"
+  "bin/Sentinel"
+  "bin/Sentinel.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sentinel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
